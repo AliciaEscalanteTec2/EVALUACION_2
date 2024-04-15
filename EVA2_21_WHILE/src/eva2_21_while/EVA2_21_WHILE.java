4a591f3f94aed0aa378c5774679dfdb07df4307c
AliciaEscalanteTec2 <159506688+AliciaEscalanteTec2@users.noreply.github.com>
2024-04-15 15:49:45 -0700

    Add files via upload

---
+++ EVA2_21_WHILE/src/eva2_21_while/EVA2_21_WHILE.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_21_while;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EVA2_21_WHILE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //FOR --> cuenta números, etc
+        //WHILE Y DO WHILE --> se controlan con condiciones
+        
+        //VER COMO FUNCIONA EL WHILE
+        int cont = 1;
+        int cont2 = 10;
+        
+        while(cont <=10){
+            System.out.print(cont + " - ");
+            cont ++; //irle sumando 1
+        }
+        System.out.println("");
+        while(cont2 >=1){
+            System.out.print(cont2 + " - ");
+            cont2 --; 
+        }
+        //ALICIA ESCALANTE SOSA   No.control=24550008
+    }
+    
+}
